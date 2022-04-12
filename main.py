e9db84abfb44538e1de46042614187ff086ee624
ZAHRAN Saeed <saeed.zahran@icm-meg-le02.icm-institute.org>
2022-04-12 13:25:52 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,5 @@
+#Epochs objects are a data structure for representing and analyzing equal-duration chunks of the EEG/MEG signal. Epochs are
+# most often used to represent data that is time-locked to repeated experimental events (such as stimulus onsets or subject button presses),
+import os
+import mne
+
