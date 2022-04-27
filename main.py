f1ed4ff13e62b067d1c865bf37c395e972b4e29f
SaeedZahran <saeedzahran@hotmail.com>
2022-04-27 20:26:41 +0200

    Update main.py

--- main.py
+++ main.py
@@ -47,6 +47,11 @@ def main():
     tmin = config.pop('t_min')
     tmax = config.pop('t_max')
     
+    
+    # crop() the Raw data to save memory:
+    raw = mne.io.read_raw_fif(data_file, verbose=False).crop(tmax=60)
+    
+    
     if 'events' in config.keys():
         events_file = config.pop('events')
         events = mne.read_events(events_file)
@@ -54,20 +59,11 @@ def main():
         # extract an events array from Raw objects using mne.find_events():
         # reading experimental events from a “STIM” channel;
         events = mne.find_events(raw, stim_channel='STI 014')
-         
-        
-    # crop() the Raw data to save memory:
-    raw = mne.io.read_raw_fif(data_file, verbose=False).crop(tmax=60)
 
     ## Build epochs parameters
-    epochs_params = dict(events, tmin=tmin, tmax=tmax)
+    epochs_params = dict(tmin=tmin, tmax=tmax)
 
-    epochs = epoch(raw,**epochs_params)
-
-
-
-
-
+    epochs = epoch(raw,events,**epochs_params)
 
 
 if __name__ == '__main__':
