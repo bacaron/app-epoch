74bff9a08d967c535f9058f72e5c79fb42f2ee25
SaeedZahran <saeedzahran@hotmail.com>
2022-05-06 08:33:56 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,28 +4,64 @@ import mne
 import json
 import os
 import matplotlib.pyplot as plt
+from pathlib import Path
+import tempfile
+import numpy as np
+import scipy.ndimage
+import matplotlib.pyplot as plt
+import mne
+
 
 # Current path
 __location__ = os.path.realpath(
     os.path.join(os.getcwd(), os.path.dirname(__file__)))
 
 
-def epoch(raw, events, tmin, tmax):
+def epoch(param_meg,param_eeg,param_eog,param_ecg,param_emg,param_stim,raw, events, tmin, tmax):
+    raw.pick_types(meg=param_meg,eeg=param_eeg,eog=param_eog,ecg=param_ecg,emg=param_emg, stim=param_stim).crop(tmax=60).load_data()
 
 
-    epochs = mne.Epochs(raw, events, tmin=tmin, tmax=tmax)
+    report = mne.Report(title='Report')
 
-    epochs.plot(n_epochs=10)
+    #raw
+    report.add_raw(raw=raw, title='Raw', psd=False)  # omit PSD plot
+
+
+    #events
+    sfreq = raw.info['sfreq']
+    report.add_events(events=events, title='Events', sfreq=sfreq)
+
+
+    #epochs
+
+    event_id = {
+        'auditory/left': 1, 'auditory/right': 2, 'visual/left': 3,
+        'visual/right': 4, 'face': 5, 'buttonpress': 32
+    }
+
+    metadata, _, _ = mne.epochs.make_metadata(
+        events=events,
+        event_id=event_id,
+        tmin=tmin,
+        tmax=tmax,
+        sfreq=raw.info['sfreq']
+    )
+    epochs = mne.Epochs(
+        raw=raw, events=events, event_id=event_id, metadata=metadata
+    )
+
+    report.add_epochs(epochs=epochs, title='Epochs from "epochs"')
+
+    # == SAVE REPORT ==
+    report.save('out_dir_report/report.html', overwrite=True)
 
     # == SAVE FILE ==
-    epochs.save(os.path.join('out_dir', 'meg-epo.fif'))
+    epochs.save(os.path.join('out_dir', 'meg-epo.fif'), overwrite=True)
 
-    # == FIGURES ==
-    plt.figure()
-    epochs.plot()
-    plt.savefig(os.path.join('out_figs', 'epochs_plot.png'))
 
-    return epochs
+
+
+
 
 
 def main():
@@ -51,10 +87,8 @@ def main():
         # reading experimental events from a “STIM” channel;
         events = mne.find_events(raw, stim_channel='STI 014')
 
-    ## Build epochs parameters
-    epochs_params = dict(tmin=tmin, tmax=tmax)
-
-    epochs = epoch(raw, events, **epochs_params)
+    print(config['param_eeg'])
+    epochs = epoch(config['param_meg'],config['param_eeg'],config['param_eog'], config['param_ecg'],config['param_emg'],config['param_stim'], raw, events, tmin=tmin, tmax=tmax)
 
 
 
